88644a05c37dbeea8fe2f9b17194f6796a169473
Serik Kumarbek <s.kumarbek@mail.ru>
2025-08-17 12:42:53 +0500

    переделал метод заказа 2

--- methods/order_methods.py
+++ methods/order_methods.py
@@ -1,10 +1,10 @@
 import requests
 from data.config_urls import CREATE_ORDER_URL, LIST_ORDERS_URL
 
-# Создание заказа
+# 1. Создание заказа
 def create_order(order_data):
     return requests.post(CREATE_ORDER_URL, json=order_data)
 
-# Получение списка заказов
+# 2. Получение списка заказов
 def get_orders_list():
     return requests.get(LIST_ORDERS_URL)
